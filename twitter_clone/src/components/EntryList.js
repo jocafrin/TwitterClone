0b3295c1045731931cc792430c6184e341140850
JoCatherine Griggs <jocafrin@gmail.com>
2019-03-10 23:43:14 -0400

    added handle, installing reactstrap. let's try it out!

--- twitter_clone/src/components/EntryList.js
+++ twitter_clone/src/components/EntryList.js
@@ -1,13 +1,18 @@
 import React from "react";
+import './EntryList.css';
+import  profImg from './profile.jpg';
 
 const EntryList = props => {
   return (
     <div className="flitList">
       {props.entries.map(flit => {
         return (
-          <div>
+          <div className="flitBox">
             <span>{flit.username}</span>
+            <span className="handle"> @{flit.username}</span>
+
             <div className="eachFlit">{flit.text}</div>
+            <img className="profImg" src={profImg} width="50" alt="banana cactus"/>
           </div>
         );
       })}
